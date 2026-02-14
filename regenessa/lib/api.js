f3f1ee6acf598a4d61ff6916e368aea45e0267e2
omoefe <raniem57@gmail.com>
2026-02-14 22:21:12 +0100

    backend url

--- regenessa/lib/api.js
+++ regenessa/lib/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
+  baseURL: "https://regenessa-backend.onrender.com/api",
   // baseURL: "http://localhost:5000/api",
 });
 
